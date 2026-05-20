23af59292219751a0c2f5020a10b6382ac1185f9
NitschMax <maximilian.nitsch@gmx.de>
2026-05-20 17:10:23 +0200

    Refactored hedger to use simulation_engine instead of stochastic_model; this separates simulation from financial logic

--- quantlib_lite/hedger/hedger.py
+++ quantlib_lite/hedger/hedger.py
@@ -1,49 +1,53 @@
-from quantlib_lite.stochastic_models import StochasticModel
+from quantlib_lite.simulation_engine import SimulationEngine
 from quantlib_lite.payoff import Payoff
 from .portfolio import Portfolio
 import numpy as np
 
 class Hedger():
-    def __init__(self, model, payoff, hedgingstrategy):
-        if isinstance(model, StochasticModel):
-            self.__model = model
+    def __init__(self, engine, payoff, hedgingstrategy):
+        if isinstance(engine, SimulationEngine):
+            self._engine = engine
         else:
-            raise TypeError('model must be an instance of StochasticModel.')
+            raise TypeError('engine must be an instance of SimulationEngine.')
         
         if isinstance(payoff, Payoff):
-            self.__payoff = payoff
+            self._payoff = payoff
         else:
             raise TypeError('payoff must be an instance of Payoff.')
 
         self.hedgingstrategy = hedgingstrategy
 
     @property
-    def model(self):
-        return self.__model
+    def engine(self):
+        return self._engine
 
     @property
     def payoff(self):
-        return self.__payoff
+        return self._payoff
 
-    def run(self, T, r, steps):
+    def run(self, r, n_paths):
         K = self.payoff.K
-        sigma = self.model.sigma
-        dt = self.model.dt(T, steps)
+        sigma = self.engine.model.sigma
+        dt = self.engine.model.dt(T, steps)
         exp_r_dt = np.exp(r * dt)
 
-        path = self.model.sample_path(T, steps)
-        S_0 = path.values[0]
+        paths = self.engine.simulate(n_paths)
+        portfolio_values, errors, S_T_array, payoff_values = np.empty((len(paths), 4))
 
-        initial_value = self.hedgingstrategy.compute_initial_value(S_0, T, r, sigma, K, self.payoff)
-        portfolio = Portfolio(cash_value = initial_value, asset_count = 0)
-        
-        for idx, (t, S_t) in enumerate(path[:-1]):
-            a_t = self.hedgingstrategy.compute_a_t(t, S_t, T, r, sigma, K, self.payoff)
-            portfolio.update(a_t, S_t)
-            portfolio.cash_value *= exp_r_dt
+        for idx, path in enumerate(paths):
+            S_0 = path.values[0]
 
-        portfolio_value = portfolio.value_at_price_S(path.values[-1])
-        payoff_value = self.payoff.evaluate(path)
-        error = portfolio_value - payoff_value
-        return portfolio_value, error, path.values[-1], payoff_value
+            initial_value = self.hedgingstrategy.compute_initial_value(S_0, T, r, sigma, K, self.payoff)
+            portfolio = Portfolio(cash_value = initial_value, asset_count = 0)
+            
+            for idx, (t, S_t) in enumerate(path[:-1]):
+                a_t = self.hedgingstrategy.compute_a_t(t, S_t, T, r, sigma, K, self.payoff)
+                portfolio.update(a_t, S_t)
+                portfolio.cash_value *= exp_r_dt
+
+            portfolio_values[idx] = portfolio.value_at_price_S(path.values[-1])
+            payoff_values[idx] = self.payoff.evaluate(path)
+            errors[idx] = portfolio_value - payoff_value
+            S_T_array[idx] = path.values[-1]
+        return portfolio_values, errors, S_T_array, payoff_values
 
